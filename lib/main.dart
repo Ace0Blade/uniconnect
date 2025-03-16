22f92e3e449a80704b23a6d7b7e4a30929501278
Ace0Blade <144112474+Ace0Blade@users.noreply.github.com>
2025-03-16 21:50:35 +0530

    update 2
    adding in files

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+import 'package:uniconnect/views/screens/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return GetMaterialApp(
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+            useMaterial3: true,
+          ),
+          home: const LoginPage(),
+        );
+      },
+    );
+  }
+}
